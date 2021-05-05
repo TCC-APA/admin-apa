912af8670e870cb00ddb865e2a6bf2b78139c66c
Fernando Godoy <nanu.godoy@hotmail.com>
2021-05-05 18:05:01 -0300

    logs

--- src/main/java/br/com/rest/api/MonitoracaoApi.java
+++ src/main/java/br/com/rest/api/MonitoracaoApi.java
@@ -1,5 +1,7 @@
 package br.com.rest.api;
 
+import java.util.Date;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,7 +14,7 @@ public class MonitoracaoApi {
 	@Path("")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String ping() {
-		return "Status de servidor: ok!";
+		return new Date() + ": Status de servidor: ok!";
 	}
 }
 
