0d9fc71b9b39ae5d42a374382bc3cd1c007b2c21
Fernando Godoy <nanu.godoy@hotmail.com>
2021-06-02 03:29:38 -0300

    bugfix get turmas

--- src/main/java/br/com/rest/api/TurmaApi.java
+++ src/main/java/br/com/rest/api/TurmaApi.java
@@ -2,8 +2,8 @@ package br.com.rest.api;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import br.com.rest.model.dto.DefaultReturn;
@@ -15,7 +15,7 @@ public class TurmaApi {
 	@GET
 	@Path("")
 	@Produces(MediaType.APPLICATION_JSON)
-	public DefaultReturn findTurmas(@PathParam(value = "idProfessor") Long idProfessor) {
+	public DefaultReturn findTurmas(@QueryParam(value = "idProfessor") Long idProfessor) {
 		return TurmaServices.buscarTurmasFiltroProfessor(idProfessor);
 		
 	}
