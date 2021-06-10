0e7e695dc365f8d045afd66061366cb9923a58f5
Fernando Godoy <nanu.godoy@hotmail.com>
2021-06-10 01:17:37 -0300

    teste deserializacao mapa

--- src/main/java/br/com/rest/api/QuestionarioApi.java
+++ src/main/java/br/com/rest/api/QuestionarioApi.java
@@ -1,7 +1,5 @@
 package br.com.rest.api;
 
-import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -9,17 +7,20 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import com.google.gson.Gson;
+
 import br.com.rest.model.dto.QuestionarioDTO;
 import br.com.rest.services.QuestionarioServices;
 
 @Path("/questionario")
 public class QuestionarioApi {
 
+	//Foi colocado o retorno como string, pois o mapa estava convertendo errado
 	@GET
 	@Path("grupos")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<QuestionarioDTO> consultarQuestionariosPorGruposAluno(@QueryParam(value = "matricula") String matricula) {
-		return QuestionarioServices.findQuestionariosPorGruposAluno(matricula);
+	public String consultarQuestionariosPorGruposAluno(@QueryParam(value = "matricula") String matricula) {
+		return new Gson().toJson(QuestionarioServices.findQuestionariosPorGruposAluno(matricula));
 	}
 	
 	@POST
