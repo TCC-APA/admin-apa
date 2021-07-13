275db1f91082c61766caf055e5263ae0d6a86a1d
Fernando Godoy <nanu.godoy@hotmail.com>
2021-07-13 01:06:06 -0300

    alteracao appconfig

--- src/main/java/br/com/rest/config/AppConfig.java
+++ src/main/java/br/com/rest/config/AppConfig.java
@@ -1,53 +1,17 @@
 package br.com.rest.config;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+public class AppConfig extends ResourceConfig {
 
-import org.reflections.Reflections;
-import org.reflections.scanners.ResourcesScanner;
-import org.reflections.scanners.SubTypesScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
-import org.reflections.util.FilterBuilder;
-
-@ApplicationPath("")
-public class AppConfig extends Application{
-
-	private Set<Class<?>> resources = new HashSet<Class<?>>();
-	
-	@SuppressWarnings("unchecked")
 	public AppConfig() {
-		
-		List<ClassLoader> classLoadersList = new LinkedList<ClassLoader>();
-		classLoadersList.add(ClasspathHelper.contextClassLoader());
-		classLoadersList.add(ClasspathHelper.staticClassLoader());
-		
-		Reflections reflections = new Reflections(new ConfigurationBuilder()
-			    .setScanners(new SubTypesScanner(false), new ResourcesScanner())
-			    .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
-			    .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix("br.com.rest.api"))));
 
-		Set<Class<? extends Object>> allClasses = 
-		     reflections.getSubTypesOf(Object.class);
-		 
-		for(Object o : allClasses) {
-			resources.add((Class<Object>) o);
-		}
+		packages("br.com.rest");
 
-		
+		register(MultiPartFeature.class);
+
 		System.out.println("AppConfig Criado!");
 	}
-	
-	@Override
-	public Set<Class<?>> getClasses() {
-		// TODO Auto-generated method stub
-		return resources;
-	}
-		
 
 }
