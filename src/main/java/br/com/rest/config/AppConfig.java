f9622a981f771018416df81c0b11d4fb12337739
Fernando Godoy <nanu.godoy@hotmail.com>
2021-07-13 01:07:44 -0300

    alteracao appconfig

--- src/main/java/br/com/rest/config/AppConfig.java
+++ src/main/java/br/com/rest/config/AppConfig.java
@@ -1,8 +1,11 @@
 package br.com.rest.config;
 
+import javax.ws.rs.ApplicationPath;
+
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
+@ApplicationPath("")
 public class AppConfig extends ResourceConfig {
 
 	public AppConfig() {
